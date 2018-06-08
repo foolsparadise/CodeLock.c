bdf721b97e949aea9f4eb55c78467be220945d9c
foolsparadise <23549415+foolsparadise@users.noreply.github.com>
2018-06-08 17:23:12 +0800

    Add files via upload

--- CodeLock.h
+++ CodeLock.h
@@ -10,6 +10,17 @@
 
 #include <pthread.h>
 
+/*
+ usage:
+ init:
+    CODELOCK ListLock;   //链锁
+    CreateCodeLock(&ListLock);
+ and:
+ CodeLock(&ListLock);
+ //do something like 比如 基于链表的push和pop这类操作
+ CodeUnlock(&ListLock);
+ */
+
 #define CODELOCK        pthread_mutex_t
 
 void CreateCodeLock(CODELOCK* pLock);
